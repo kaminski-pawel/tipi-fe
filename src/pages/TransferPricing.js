ca0c67859bfd682805a50659a7fad1f7f87523cd
Pawel Kaminski <pawelkaminski@tipi.software>
2021-07-01 12:45:09 +0200

    Add base for i18n

--- src/pages/TransferPricing.js
+++ src/pages/TransferPricing.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TransferPricing from "../components/tp/TransferPricing.jsx";
+
+import "../i18n";
 
 ReactDOM.render(
   <React.StrictMode>
     <React.Suspense fallback={<div>Proszę chwilę zaczekać...</div>}>
-      <p>TransferPricing</p>
+      <TransferPricing
+        lang={
+          JSON.parse(document.getElementById("backend-options").textContent)
+            .lang
+        }
+      />
     </React.Suspense>
   </React.StrictMode>,
   document.getElementById("react-app-root")
