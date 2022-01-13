3b075e120ea5820902743807847fac6c4c1c8820
Pawel Kaminski <pawelkaminski@tipi.software>
2022-01-13 01:34:12 +0100

    Add css and styles loaders

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
         loader: "babel-loader",
         options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
